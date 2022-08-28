0ca5baee22f76b8896025acfd2e7e3824ed8d3b6
alessandromasone <masonealessandro04@gmail.com>
2022-08-28 12:14:41 +0200

    Parametri
    Modifica completa del programma, parte riguardante i parametri (analisi e controllo).

--- main.py
+++ main.py
@@ -1,90 +1,59 @@
-#Masone Alessandro
-import requests
-import hashlib
-import shutil
-import os
-import time
+#!/usr/bin/python
+
+import argparse
 import sys
-from datetime import datetime
+import requests
+from urllib.parse import urlparse
 
+#versione del programma
+VERSION = 1.0
 
-#controllo del parametro di avvio
-if (sys.argv[1] == 'help' or sys.argv[1] == '-h'):
-    print('Utilizzo: ' + sys.argv[0] + " <link dell'immagine>")
-    exit(0)
+#tempo di attesa per ogni controllo e quante volte ripetere (-1 = infinito)
+TIME = 5
+REPEAT = -1
 
-#passaggio del link attraverso il passaggio si parametri
-link = sys.argv[1]
+#controllo dei parametri
+def check_argv():
+    parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + str(VERSION), help="Versione del programma")
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Visualizza i comandi')
+    parser.add_argument("-t", help="Ogni quanto controllare", default=TIME, dest="time", type=int)
+    parser.add_argument("-r", help="Quante volte controllare", default=REPEAT, dest="repeat", type=int)
+    parser.add_argument("url", help="Url da monitorare")
+    args = parser.parse_args()
+    return args
 
-#nome temporaneo per il controllo
-name_temp = ''
-path_temp = 'temp/'
+#controllo url
+def is_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
-#nome dell'immagine da salvare
-name_save = ''
-path_save = 'save/'
+if __name__ == "__main__":
+    argv = check_argv()
+    #controllo varoli inserti
+    #tempo
+    if (int(argv.time) < 5 or int(argv.time) > 2592000):
+        print("Il tempo non deve essere minore di 5 o maggiore di 2592000")
+        sys.exit()
+    #cicli da ripetere
+    if (int(argv.repeat) < -1 or int(argv.repeat) > 999999999):
+        print("Il numero delle volte da ripete è troppo alto")
+        sys.exit()
+    #controllo formato dell'url
+    if (not is_url(argv.url)):
+        print("Il sito sembra non essere valido")
+        sys.exit()
+    #controllo eisstenza dell'url
+    response = requests.get(argv.url)
+    if (response.status_code != 200):
+        print("Il sito sembra non essere raggiungibile")
+        sys.exit()
+    #presa dei valori
+    time = int(argv.time)
+    repeat = int(argv.repeat)
+    url = argv.url
 
-#nome dell'immagine nella home
-image_view_name = 'image.jpg'
-
-#tempo di attesa per il contorllo
-time_check = 5
-
-
-#funzione di debug dei messaggi
-def debug(string, status = False):
-    if(status):
-        print('[' + datetime.now().strftime('%H:%M:%S') + '] ' + string)
-
-#controlo con md5 del file
-def get_hash(img_path):
-    with open(img_path, "rb") as f:
-        img_hash = hashlib.md5()
-        while chunk := f.read(8192):
-            img_hash.update(chunk)
-    debug('Hash ' + img_path + ' = ' + img_hash.hexdigest())
-    return img_hash.hexdigest()
-
-#download dell'immagine per il controllo
-def get_image(image_url, path):
-    img_data = requests.get(image_url).content
-    with open(path, 'wb') as handler:
-        handler.write(img_data)
-    debug('Download immagine dal link ' + image_url)
-
-#loop del programma
-while True:
-    #controllo esistenza cartella temp
-    if not os.path.exists(path_temp):
-        os.makedirs(path_temp)
-        debug('Creazione cartella ' + path_temp)
-
-    #creazione del nome dell'immagine temporanea per il controllo e download
-    name_temp = 'image_' + datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + '.jpg'
-    get_image(link, path_temp + name_temp)
-
-    #se non esiste un immagine
-    if (name_save == ''):
-        name_save = name_temp
-        shutil.copy2(path_temp + name_temp, image_view_name)
-        debug('Copia ' + path_temp + name_temp + ' in ' + image_view_name)
-    #controllo se è una nuova immagine
-    elif (get_hash(path_temp + name_temp) != get_hash(image_view_name)):
-        #salvataggio nell'immagine
-        debug('Nuova immagine trovata')
-        name_save = name_temp
-        #controllo esistenza cartella save
-        if not os.path.exists(path_save):
-            os.makedirs(path_save)
-            debug('Creazione cartella save')
-        shutil.copy2(image_view_name, path_save + name_save)
-        debug('Copia ' + image_view_name + ' in ' + path_save + name_save)
-        shutil.copy2(path_temp + name_temp, image_view_name)
-        debug('Copia ' + path_temp + name_temp + ' in ' + image_view_name)
-
-    #pulizia file temporanei
-    if os.path.exists(path_temp + name_temp):
-        os.remove(path_temp + name_temp)
-
-    #attesa ciclo
-    time.sleep(time_check)
